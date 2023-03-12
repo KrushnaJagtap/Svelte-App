c05a9a3ac01c5ab9fc3d3ebb6d988bf7f3fd19f7
Krushna Jagtap <86608386+KrushnaJagtap@users.noreply.github.com>
2023-03-12 17:49:44 +0530

    modified:   src/App.svelte 	src/components/Content.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,25 +1,14 @@
 <script>
 	import Header from "./components/Header.svelte";
 	import Footer from "./components/Footer.svelte";
+	import Content from "./components/Content.svelte";
 </script>
 <Header/>
-
-<main>
-	<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor esse repellendus ipsa nisi, 
-		est praesentium velit voluptatem numquam sit, 
-		quisquam laudantium ratione odit. Animi quidem aspernatur ipsa minus iusto rem.
-</main>
-
 <Footer/>
+<Content/>
 
 
 
-<style>
-	main{
-		max-width: 960px;
-		margin: 40px auto;
-	}
 
-</style>
 
 
