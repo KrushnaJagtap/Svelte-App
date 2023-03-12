1fde76dfb90b0e196ffc5110f57290a13dadd81b
Krushna Jagtap <86608386+KrushnaJagtap@users.noreply.github.com>
2023-03-12 19:57:35 +0530

    modified:   src/components/Header.svelte

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -6,6 +6,12 @@
     <h1>
       <img src="/img/applogo.jpg " alt="applogo">
     </h1>
+    <button
+        id="navAction"
+        class="mx-auto lg:mx-0 hover:underline bg-white text-gray-800 font-bold rounded-full mt-4 lg:mt-0 py-4 px-8 shadow opacity-75 focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out"
+      >
+        Action
+      </button>
     
 </header>
 
