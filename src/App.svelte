2686788fc3be5ec02015f4ec0f9a4b2afc70c5c2
Krushna Jagtap <86608386+KrushnaJagtap@users.noreply.github.com>
2023-03-12 17:08:43 +0530

    modified:   src/App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -4,7 +4,18 @@
 </script>
 <Header/>
 <Footer/>
+
+<main>
+	<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor esse repellendus ipsa nisi, 
+		est praesentium velit voluptatem numquam sit, 
+		quisquam laudantium ratione odit. Animi quidem aspernatur ipsa minus iusto rem.
+</main>
+
 <style>
+	main{
+		max-width: 960px;
+		margin: 40px auto;
+	}
 
 </style>
 
