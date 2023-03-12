901c2398030ccd1fade269d25a8b7d20495bda77
Krushna Jagtap <86608386+KrushnaJagtap@users.noreply.github.com>
2023-03-12 17:50:00 +0530

    new file:   src/components/Content.svelte

---
+++ src/components/Content.svelte
@@ -0,0 +1,12 @@
+<main>
+	<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor esse repellendus ipsa nisi, 
+		est praesentium velit voluptatem numquam sit, 
+		quisquam laudantium ratione odit. Animi quidem aspernatur ipsa minus iusto rem.
+</main>
+<style>
+	main{
+		max-width: 960px;
+		margin: 40px auto;
+	}
+
+</style>
