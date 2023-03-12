9dbac93e2a9ffc964be520e51985ad44015bc7e7
Krushna Jagtap <86608386+KrushnaJagtap@users.noreply.github.com>
2023-03-12 17:09:49 +0530

    modified:   src/App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -3,13 +3,16 @@
 	import Footer from "./components/Footer.svelte";
 </script>
 <Header/>
-<Footer/>
 
 <main>
 	<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor esse repellendus ipsa nisi, 
 		est praesentium velit voluptatem numquam sit, 
 		quisquam laudantium ratione odit. Animi quidem aspernatur ipsa minus iusto rem.
 </main>
+
+<Footer/>
+
+
 
 <style>
 	main{
