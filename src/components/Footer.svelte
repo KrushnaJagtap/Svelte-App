5a532da2089f59c48c72ed9e34c91b2cbceccf60
Krushna Jagtap <86608386+KrushnaJagtap@users.noreply.github.com>
2023-03-12 10:54:12 +0530

    modified:   src/App.svelte 	new file:   src/components/Footer.svelte

---
+++ src/components/Footer.svelte
@@ -0,0 +1,20 @@
+<footer>
+    <div class="copyright">Copyright 2023 Integrated Services</div>
+</footer>
+
+<style>
+    footer{
+        padding: 50px;
+        text-align: center;
+    }
+    .copyright{
+        color: #aaa;
+        font-size: 14px;
+        display: inline-block;
+        padding: 20px;
+        border-top: 1px solid#ddd;
+
+
+    }
+    
+</style>
